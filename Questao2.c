a9f50d427d8a8ffc8c3cc4dfa696fc47eac8f6a5
h3nriqx <142359662+Cauahenr178@users.noreply.github.com>
2023-12-01 11:59:53 -0300

    investimento

---
+++ Questao2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    
+  int numMeses;
+    double investimentoInicial, taxaJuros;
+
+    scanf("%d", &numMeses);
+    scanf("%lf", &investimentoInicial);
+    scanf("%lf", &taxaJuros); 
+    
+    double montante = 0;
+    for (int i = 1; i <= numMeses; i++) {
+        montante += investimentoInicial;
+        montante *= (1 + taxaJuros);
+         
+         printf("Montante ao fim do mes %d: R$ %.2lf\n", i, montante);
+    
+}
+
+    return 0;
+}
